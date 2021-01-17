88c6477cb206f5ed0340f6b37a4f3f6c27fedf6a
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-01-17 14:01:13 +0100

    Add files via upload

---
+++ 2.Zadatak/2.Zadatak/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Zadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj;
+
+            Console.Write("Upisite jedan broj: ");
+            broj = Convert.ToInt32(Console.ReadLine());
+
+            if (broj % 4 == 0 && broj%6 !=0)
+            {
+                Console.WriteLine("Broj je djeljiv s 4");
+            }
+            if (broj % 6 == 0 && broj%4 !=0)
+            {
+                Console.WriteLine("Broj je djeljiv s 6");
+            }
+            if (broj % 4 == 0 && broj % 6 == 0)
+            { Console.WriteLine("Broj je djeljiv sa 4 i sa 6"); 
+            }
+            if (broj % 4 != 0 && broj % 6 != 0)
+            {
+                Console.WriteLine("Broj nije dijeljiv s 4 i 6 ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
